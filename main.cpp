56e43aa21213bd1b3f2b1f00abf5699190be5bf7
Beren Millidge <bmillidgework@gmail.com>
2018-08-17 12:54:56 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -7,6 +7,7 @@
 #include "sphere.hpp"
 #include "hitable_list.hpp"
 #include "float.h" // what is this weird float.h file!?
+#include "camera.hpp"
 using namespace std;
 
 // okay, so just trace out a pattern... see if it works?
@@ -123,23 +124,22 @@ int main_old() {
 	return 0;
 }
 
-int main() {
+int main_without_camera() {
 	int nx = 200;
 	int ny = 100;
 	ofstream myfile;
 	myfile.open("hitable_world.txt");
 	myfile << "P3\n" << nx << " " << ny << "\n255\n";
-	vec3 lower_left_corner(-2.0, -1.0, -1.0);
-	vec3 horizontal(4.0, 0.0,0.0);
-	vec3 vertical(0.0, 2.0,0.0);
-	vec3 origin(0.0,0.0,0.0);
 	hitable *list[2]; // create a new hitable list
 	list[0] = new sphere(vec3(0,0,-1), 0.5);
 	list[1] = new sphere(vec3(0,-100.5,-1), 100);
 	hitable *world = new hitable_list(list, 2);
+	vec3 lower_left_corner(-2.0, -1.0, -1.0);
+	vec3 horizontal(4.0, 0.0,0.0);
+	vec3 vertical(0.0, 2.0,0.0);
+	vec3 origin(0.0,0.0,0.0);
 	for (int j = ny-1; j>=0; j--){
 		for (int i = 0; i<nx; i++){
-			// iteratethrough all the things as normal
 			float u = float(i)/ float(nx);
 			float v = float(j) / float(ny);
 
@@ -156,3 +156,44 @@ int main() {
 	return 0;
 }
 // oh my god... that actually worked... that's crazy!
+
+int main_with_camera() {
+	int nx = 200;
+	int ny = 100;
+	ofstream myfile;
+	myfile.open("camera_samples.txt");
+	myfile << "P3\n" << nx << " " << ny << "\n255\n";
+	hitable *list[2]; // create a new hitable list
+	list[0] = new sphere(vec3(0,0,-1), 0.5);
+	list[1] = new sphere(vec3(0,-100.5,-1), 100);
+	hitable *world = new hitable_list(list, 2);
+	camera cam;
+	int num_samples = 100;
+	for (int j = ny-1; j>=0; j--){
+		for (int i = 0; i<nx; i++){
+			// define starting color
+			vec3 col(0,0,0);
+			// now get a number of samples for each pixel
+			for (int s = 0; s < num_samples; s++) {
+				float u = float(i + drand48())/ float(nx);
+				float v = float(j + drand48()) / float(ny);
+				ray r = cam.get_ray(u,v);
+				vec3 p = r.point_at_parameter(2.0); // why arewe calculating this here? it's not used?
+				col += color_hitable_world(r, world);
+			}
+			col /= float(num_samples);
+			int ir = int(255.99*col[0]);
+			int ig = int(255.99*col[1]);
+			int ib = int(255.99*col[2]);
+			myfile << ir << " " << ig << " " << ib << "\n";
+		}
+	}
+	myfile.close();
+	return 0;
+	// fantastically this works as well... this is amazing!
+
+}
+
+int main() {
+	return main_with_camera();
+}
